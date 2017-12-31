1ad2d96758d59751aecb764f7ebbfa7a367b8707
Nick Chisiu <nick@squeezer.io>
2017-12-31 12:54:24 +0200

    added exchangers info

--- src/components/token-sale/stats.js
+++ src/components/token-sale/stats.js
@@ -1,8 +1,9 @@
 import CountUp from 'react-countup';
-import { rhythm, scale } from "../utils/typography"
+import { rhythm, scale } from "../../utils/typography"
 import TrackVisibility from 'react-on-screen';
-import NpmIcon from "../assets/npm.png"
-import DeveloperIcon from "../assets/developer.png"
+import NpmIcon from "../../assets/npm.png"
+import DeveloperIcon from "../../assets/developer.png"
+import ExchangerIcon from "../../assets/exchanger.png"
 
 const DownloadsCounter = ({ isVisible }) => {
   return (
@@ -27,16 +28,23 @@ export default () => {
       lineHeight: 1,
       fontSize: scale(1).fontSize,
     }} offset={300}>
-      <div css={{display: 'inline-block', margin: '25px', height: '300px', verticalAlign: 'middle'}}>
+      <div css={{ display: 'inline-block', margin: '25px', height: '300px', verticalAlign: 'middle' }}>
         + 75000 monthly downloads
         <div css={{ cursor: 'pointer', marginTop: rhythm(3) }} onClick={() => window.open("https://www.npmjs.com/package/squeezer-cli", "_blank")}>
           <img css={{ width: '180px' }} src={NpmIcon} alt="play video" />
         </div>
       </div>
-      <div css={{display: 'inline-block', margin: '25px', height: '300px', verticalAlign: 'middle'}}>
+      <div css={{ display: 'inline-block', margin: '25px', height: '300px', verticalAlign: 'middle' }}>
         + 40 mil targeted developers
         <div css={{ cursor: 'pointer', marginTop: rhythm(3) }} onClick={() => window.open("https://www.npmjs.com/package/squeezer-cli", "_blank")}>
           <img css={{ width: '180px' }} src={DeveloperIcon} alt="play video" />
+        </div>
+      </div>
+      <div css={{ display: 'inline-block', margin: '25px', height: '300px', verticalAlign: 'middle' }}>
+        + 7000 exchangers
+        <div css={{ cursor: 'pointer', marginTop: rhythm(3) }} onClick={() => window.open("https://www.npmjs.com/package/squeezer-cli", "_blank")}>
+
+          <img css={{ width: '180px' }} src={ExchangerIcon} alt="play video" />
         </div>
       </div>
     </div>
