0be9b9d0b0db278523b3ed557d78022cfb61d758
Nick Chisiu <nick@squeezer.io>
2018-02-18 22:10:58 +0200

    added major changes #7

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,7 +10,7 @@ if (typeof localStorage !== "undefined") {
 
 // axios.interceptors.response.use((response) => {
 //   return response;
-// }, function (error) {
+// }, function (error) { 
 //   console.log(response)
 //   if (error.response.status === 401) {
 //     window.location = "/token-sale/account/login/"
