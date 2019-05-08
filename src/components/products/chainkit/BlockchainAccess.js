7c11c4ef250559bf2da1e668a83c99ccf24dca9c
Nick Chisiu <nick@squeezer.io>
2019-05-08 21:25:45 +0300

    added site doc

--- src/components/products/chainkit/BlockchainAccess.js
+++ src/components/products/chainkit/BlockchainAccess.js
@@ -5,6 +5,7 @@ import blockchainImage from '../../../../static/images/Squeezer_Assets_chainkit_
 import bidirectionalImage from '../../../../static/images/Squeezer_Assets_chainkit_page/1298360-256-bidirectional-payment.svg';
 import smartContactImage from '../../../../static/images/Squeezer_Assets_chainkit_page/2647413-128-smart-contract.svg';
 import walletSecurityImage from '../../../../static/images/Squeezer_Assets_chainkit_page/3453534-128-wallet-security.svg';
+import chainImage from '../../../../static/images/Squeezer_Assets_chainkit_page/chainicon.png';
 
 export default class BlockchainAccess extends Component {
   renderService = (title, image, description) => (
@@ -20,15 +21,16 @@ export default class BlockchainAccess extends Component {
       }
     }}>
       <div css={{ display: 'flex' }}>
-        <img src={image} alt='image' css={{marginRight: '1rem', marginBottom: 0, height: '64px', width: '64px'}} />
+        <img src={image} alt='image' css={{ marginRight: '1rem', marginBottom: 0, height: '64px', width: '64px' }} />
         <h3 css={{
-          width: '271px', color: '#023775',	fontFamily: 'DIN Condensed',	fontSize: '25px',	fontWeight: 'bold', letterSpacing: '.87px', lineHeight: '30px', marginTop: '-5px', whiteSpace: 'no-wrap',
-          [presets.Phablet]: {marginTop: '20px'}
+          width: '271px', color: '#023775', fontFamily: 'DIN Condensed', fontSize: '25px', fontWeight: 'bold', letterSpacing: '.87px', lineHeight: '30px', marginTop: '-5px', whiteSpace: 'no-wrap',
+          [presets.Phablet]: { marginTop: '20px' }
         }}>{title}</h3>
       </div>
-      <p css={{color: '#023775', fontFamily: 'Khula', fontSize: '18px', letterSpacing: '.87px', lineHeight: '24px', height: '48px', width: '411px', fontWeight: 200, marginLeft: '80px', marginTop: '-25px',
-        [presets.Phablet]: {width: '300px', marginTop: '20px', marginLeft: '-4px', textAlign: 'center'},
-        [presets.Mobile]: {width: '280px'}
+      <p css={{
+        color: '#023775', fontFamily: 'Khula', fontSize: '18px', letterSpacing: '.87px', lineHeight: '24px', height: '48px', width: '411px', fontWeight: 200, marginLeft: '80px', marginTop: '-25px',
+        [presets.Phablet]: { width: '300px', marginTop: '20px', marginLeft: '-4px', textAlign: 'center' },
+        [presets.Mobile]: { width: '280px' }
       }}>
         {description}
       </p>
@@ -37,13 +39,15 @@ export default class BlockchainAccess extends Component {
 
   render() {
     return (
-      <div css={{ minHeight: '90vh', marginTop: '20rem', [presets.Hd]: {minHeight: '30vh'}, [presets.Phablet]: {minHeight: '20vh'}}}>
-        <div css={{margin: '0 auto', maxWidth: '1200px', position: 'relative'}}>
-          <div css={{position: 'absolute', width: '100%', zIndex: '200', [presets.Hd]: { marginTop: '100px'}, [presets.Desktop]: { marginTop: '0px'},
-            [presets.Desktop]: {position: 'relative'}
+      <div css={{ minHeight: '90vh', marginTop: '20rem', [presets.Hd]: { minHeight: '30vh' }, [presets.Phablet]: { minHeight: '20vh' } }}>
+        <div css={{ margin: '0 auto', maxWidth: '1200px', position: 'relative' }}>
+          <div css={{
+            position: 'absolute', width: '100%', zIndex: '200', [presets.Hd]: { marginTop: '100px' }, [presets.Desktop]: { marginTop: '0px' },
+            [presets.Desktop]: { position: 'relative' }
           }}>
-            <div css={{height: '100%', width: '520px', position: 'relative',  marginLeft: '150px', marginTop: '-40px',[presets.Hd]:{width: 'auto'},
-              [presets.Phablet]: {marginLeft: '0'}
+            <div css={{
+              height: '100%', width: '520px', position: 'relative', marginLeft: '150px', marginTop: '-40px', [presets.Hd]: { width: 'auto' },
+              [presets.Phablet]: { marginLeft: '0' }
             }}>
               <div css={{
                 height: '279px', width: '514px', borderRadius: '1px', backgroundColor: 'transparent', border: '2px solid #06DD72',
@@ -95,19 +99,22 @@ export default class BlockchainAccess extends Component {
                 }
               }}
               >
-                VERSATILE ACCESS<br/>TO BLOCKCHAIN SERVICES
+                VERSATILE ACCESS<br />TO BLOCKCHAIN SERVICES
               </div>
             </div>
-            <div css={{position: 'relative', width: '100%', display: 'flex', flexWrap: 'wrap', marginTop: '106px', paddingLeft: '68px',
-              [presets.Hd]: {marginTop: '290px'}, [presets.Desktop]: {marginTop: '490px'}, [presets.Phablet]: {padding: 0, marginTop: '250px'}
+            <div css={{
+              position: 'relative', width: '100%', display: 'flex', flexWrap: 'wrap', marginTop: '76px', paddingLeft: '68px',
+              [presets.Hd]: { marginTop: '290px' }, [presets.Desktop]: { marginTop: '490px' }, [presets.Phablet]: { padding: 0, marginTop: '250px' }
             }}>
-              <div css={{display: 'flex', flex: 1, flexDirection: 'column', flexWrap: 'wrap', alignContent: 'center'}}>
-                {this.renderService('Wallet Security', walletSecurityImage, 'Chainkit empowers the end user to manage and store his own private credentials.')}
-                {this.renderService('Bidirectional Payment Channels', bidirectionalImage, 'Get integration for both inbound and outbound onchain transactions.')}
+              <div css={{ display: 'flex', flex: 1, flexDirection: 'column', flexWrap: 'wrap', alignContent: 'center' }}>
+                {this.renderService('Wallet Security', walletSecurityImage, 'Chainkit empowers the end user to manage and store his own private credentials, additionally double wallet encryption is enabled by default.')}
+                {this.renderService('Instant Chain Transactions', chainImage, 'Transactions are made instantly without waiting the blockchain confirmations or other delay issues.')}
               </div>
-
-              <div css={{display: 'flex', flex: 1, flexDirection: 'column', flexWrap: 'wrap', alignContent: 'center', [presets.Desktop]: {marginLeft: 0}}}>
+              <div css={{ display: 'flex', flex: 1, flexDirection: 'column', flexWrap: 'wrap', alignContent: 'center', [presets.Desktop]: { marginLeft: 0 } }}>
+                {this.renderService('Bidirectional Payment Channels', bidirectionalImage, 'Get integration for both inbound and outbound onchain transactions.')}
                 {this.renderService('Blockchain agnostic', blockchainImage, 'We help you access different blockchains or switch between them with only one line of code.')}
+              </div>
+              <div css={{ display: 'flex', flex: 1, flexDirection: 'column', flexWrap: 'wrap', alignContent: 'left', [presets.Desktop]: { marginLeft: 0 } }}>
                 {this.renderService('Smart contract secure access', smartContactImage, 'Squeezer adds an extra layer on the top of your smart contract to protect private methods and make it easier to access.')}
               </div>
             </div>
@@ -130,13 +137,13 @@ export default class BlockchainAccess extends Component {
               marginTop: '300px'
             },
             [presets.Desktop]: {
-             display: 'none'
+              display: 'none'
             }
           }}>
           {/*<img src={chainKitImage} alt="chainKitImage" css={{width: '510px', opacity: 1,height: '510px', marginLeft: '5px', marginTop: '30px', position: 'relative', zIndex: 200, [presets.Hd]: { display: 'none'}}}/>*/}
 
         </div>
-        <img src={chainKitImage} alt="chainKitImage" css={{width: '510px', height: '510px', marginLeft: '775px', marginTop: '-630px', position: 'absolute', zIndex: 200, [presets.Hd]: { display: 'none'}}}/>
+        <img src={chainKitImage} alt="chainKitImage" css={{ width: '510px', height: '510px', marginLeft: '775px', marginTop: '-630px', position: 'absolute', zIndex: 200, [presets.Hd]: { display: 'none' } }} />
       </div>
     );
   }
